09e1b9a0c5540dabe1c80fd7b36bdabd70a494c1
BT_1N3_01 <mahmed9010@bths.edu>
2018-11-30 11:29:31 -0500

    dfhfgh

---
+++ src/Runner.java
@@ -0,0 +1,8 @@
+public class Runner {
+    public static void main(String[] args)
+    {
+        SortCompetition team1 = new Team1SortCompetition();
+        int[] randIntArr = randomIntsArr(10000,5);
+        String[] randStringArr =
+    }
+}
